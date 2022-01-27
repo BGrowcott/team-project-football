21aed46cb431d4443253386e12ff1401b007e91e
Ben <bgrowcott@hotmail.co.uk>
2022-01-27 20:53:25 +0000

    testing adding api to webpage

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
 let footballData = "";
 
+// get data on each Premier League Team
 fetch("https://api.football-data.org/v2/competitions/PL/teams", {
   headers: { "X-Auth-Token": "b7bac95afb44489a83d8eb77fc894151" },
 })
@@ -9,18 +10,24 @@ fetch("https://api.football-data.org/v2/competitions/PL/teams", {
   .then(function (data) {
     footballData = data;
   })
-  .then(renderCrests);
+  .then(renderCrests)
+  .then(renderTeamInfo);
 
+
+// adds all crests to the welcome page
 function renderCrests() {
   for (i = 0; i < footballData.teams.length; i++) {
     $("#crestContainer").append(
       $(
-        `<img class='crests' alt='Crest for ${footballData.teams[i].name} 'title='${footballData.teams[i].name}' src=${footballData.teams[i].crestUrl}>`
+        `<img class='crests' alt='Crest for ${footballData.teams[i].name} 
+        'title='${footballData.teams[i].name}' src=${footballData.teams[i].crestUrl}>`
       )
     );
   }
 }
 
-function getTeamInfo(){
-    
+// testing function for add individual crest to page
+function renderTeamInfo() {
+    $('#teamCrest').attr('src', `${footballData.teams[0].crestUrl}`)
+    $('#teamName').text(`${footballData.teams[0].name}`)
 }
