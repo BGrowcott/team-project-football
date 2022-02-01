c22ccf3cb01ad2a9f1f085134bf083109ca87078
Ben <bgrowcott@hotmail.co.uk>
2022-02-01 18:15:28 +0000

    made some js neater - added a couple of functions for fixtures render

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ let standings;
 let tagsArray = [];
 let fixturesData;
 let crestButtonArray;
+const searchInput = $("#teamSearch");
 
 // get data on each Premier League Team
 fetch("https://api.football-data.org/v2/competitions/PL/teams", {
@@ -36,7 +37,7 @@ function renderTeamInfo(e) {
   renderTeam();
   renderLeagueStats();
   fetchFixtures();
-  $("#teamSearch").val("");
+  searchInput.val("");
 }
 
 // event listener for search button
@@ -50,7 +51,7 @@ function fillTagsArray() {
 }
 // jQuery UI autocomplete widget
 $(function () {
-  $("#teamSearch").autocomplete({
+  searchInput.autocomplete({
     source: tagsArray,
   });
 });
@@ -60,8 +61,8 @@ function renderTeam() {
   for (let i = 0; i < teamData.teams.length; i++) {
     const team = teamData.teams[i];
     if (
-      $("#teamSearch").val().toUpperCase() === team.name.toUpperCase() ||
-      $("#teamSearch").val().toUpperCase() === team.shortName.toUpperCase()
+      searchInput.val().toUpperCase() === team.name.toUpperCase() ||
+      searchInput.val().toUpperCase() === team.shortName.toUpperCase()
     ) {
       selectedTeamId = team.id;
       $("#teamCrest").attr("src", `${team.crestUrl}`);
@@ -93,13 +94,13 @@ const urlParams = new URLSearchParams(window.location.search);
 function loadTeamFromWelcomePage() {
   for (let i = 0; i < 20; i++) {
     if (parseInt(urlParams.get("id"), 10) === teamData.teams[i].id) {
-      $("#teamSearch").val(teamData.teams[i].name);
+      searchInput.val(teamData.teams[i].name);
     }
   }
   renderTeam();
   renderLeagueStats();
   fetchFixtures();
-  $("#teamSearch").val("");
+  searchInput.val("");
 }
 
 // fetch upcoming fixtures
@@ -124,6 +125,9 @@ function renderFixtures() {
   $("#fixturesContainer").empty();
   for (i = 0; i < 10; i++) {
     const fixture = fixturesData.matches[i];
+    if (fixture.competition.name !== "Premier League") {
+      continue;
+    }
     $("#fixturesContainer").append(
       $(
         `<div class="gameContainer">
@@ -131,9 +135,25 @@ function renderFixtures() {
             <ul>
               <li>${moment
                 .utc(fixture.utcDate)
-                .format("DD/MM/YYYY - HH:mm")}</li>
-              <li>${fixture.competition.name}</li>
-              <li>${fixture.homeTeam.name} vs ${fixture.awayTeam.name}</li>
+                .format("MMMM Do YYYY - HH:mm")}</li>
+              <li><h3>${fixture.competition.name}</h3></li>
+              <li>
+                <span> 
+                  ${fixture.homeTeam.name}
+                </span>
+              </li>
+              <li>
+                <img class='fixtureCrest' src=${homeTeamCrest(fixture)}>
+                <span> 
+                 VS 
+                </span>
+                <img class='fixtureCrest' src=${awayTeamCrest(fixture)}>
+              </li>
+              <li>
+                <span> 
+                ${fixture.awayTeam.name}
+                </span>
+              </li>
               <li>${stadiumChooser(fixture)}</li>
             </ul>
           </div>
@@ -146,6 +166,7 @@ function renderFixtures() {
   }
 }
 
+// function for identifying fixture venue
 function stadiumChooser(fixture) {
   let homeTeam = fixture.homeTeam.id;
   for (let team of teamData.teams) {
@@ -155,18 +176,37 @@ function stadiumChooser(fixture) {
   }
 }
 
-const fixtureToggleButton = $("#showFixtures")
-const fixturesContainer = $("#fixturesContainer")
+function homeTeamCrest(fixture) {
+  let homeTeam = fixture.homeTeam.id;
+  for (let team of teamData.teams) {
+    if (team.id === homeTeam) {
+      return team.crestUrl;
+    }
+  }
+}
+
+function awayTeamCrest(fixture) {
+  let awayTeam = fixture.awayTeam.id;
+  for (let team of teamData.teams) {
+    if (team.id === awayTeam) {
+      return team.crestUrl;
+    }
+  }
+}
+
+// toggle fixture display
+const fixtureToggleButton = $("#showFixtures");
+const fixturesContainer = $("#fixturesContainer");
 fixtureToggleButton.click(toggleFixturesDisplay);
 
 function toggleFixturesDisplay() {
   if (fixturesContainer.css("display") === "none") {
     fixturesContainer.css("display", "flex");
-    fixtureToggleButton.text('Hide Fixtures')
-    return
+    fixtureToggleButton.text("Hide Fixtures");
+    return;
   }
   if (fixturesContainer.css("display") === "flex") {
     fixturesContainer.css("display", "none");
-    fixtureToggleButton.text('Show Fixtures')
+    fixtureToggleButton.text("Show Fixtures");
   }
 }
